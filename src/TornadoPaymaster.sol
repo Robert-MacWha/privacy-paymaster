2b21d587305d1671e70f46ccdef3639fb9809db2
Robert MacWha <github@macwha.com>
2026-04-09 11:55:29 -0400

    feat: fee

--- src/TornadoPaymaster.sol
+++ src/TornadoPaymaster.sol
@@ -22,11 +22,11 @@ contract TornadoPaymaster is IAccount, BasePaymaster {
     error NullifierAlreadySpent(bytes32 nullifierHash);
     error UnknownRoot(bytes32 root);
     error InvalidProof();
+    error ForwardFailed();
 
-    // ----- STATE -----
+    // ----- CONSTANTS -----
     ITornadoInstance public immutable TORNADO_INSTANCE;
-    bytes4 public constant WITHDRAW_FUNCTION_SELECTOR =
-        ITornadoInstance.withdraw.selector;
+    uint256 public constant POST_OP_GAS_OVERHEAD = 10_000;
 
     // ----- CONSTRUCTOR -----
     constructor(
@@ -39,17 +39,24 @@ contract TornadoPaymaster is IAccount, BasePaymaster {
 
     // ----- IAccount -----
     function validateUserOp(
-        PackedUserOperation calldata userOp,
-        bytes32 /* userOpHash */,
-        uint256 /* missingAccountFunds */
+        PackedUserOperation calldata,
+        bytes32,
+        uint256
     ) external virtual override returns (uint256 validationData) {
         _requireFromEntryPoint();
-        _withdrawFromTornado(userOp.callData, false);
         validationData = 0;
     }
 
-    fallback() external {
-        _withdrawFromTornado(msg.data, true);
+    /// @notice Entry point calls this function to execute the withdrawal after
+    /// validating the proof and other params in `validatePaymasterUserOp`.
+    function withdraw(
+        bytes calldata proof,
+        bytes32 root,
+        bytes32 nullifierHash,
+        address destination,
+        uint256 refund
+    ) external {
+        _withdrawFromTornado(proof, root, nullifierHash, refund, true);
     }
 
     // ----- Paymaster -----
@@ -64,38 +71,48 @@ contract TornadoPaymaster is IAccount, BasePaymaster {
         returns (bytes memory context, uint256 validationData)
     {
         if (userOp.sender != address(this)) revert SenderNotSelf();
+        if (bytes4(userOp.callData[:4]) != this.withdraw.selector)
+            revert InvalidSelector();
 
-        context = "";
+        (, , , address destination, ) = abi.decode(
+            userOp.callData[4:],
+            (bytes, bytes32, bytes32, address, uint256)
+        );
+
+        //? Store the destination in the context to be used in _postOp.
+        context = abi.encode(destination);
         validationData = 0;
     }
 
     function _postOp(
-        IPaymaster.PostOpMode,
-        bytes calldata,
-        uint256,
-        uint256
-    ) internal override {}
+        IPaymaster.PostOpMode mode,
+        bytes calldata context,
+        uint256 actualGasCost,
+        uint256 actualUserOpFeePerGas
+    ) internal override {
+        if (mode == IPaymaster.PostOpMode.postOpReverted) return;
+
+        address payable destination = payable(abi.decode(context, (address)));
+
+        uint256 denomination = TORNADO_INSTANCE.denomination();
+        uint256 fee = actualGasCost +
+            (POST_OP_GAS_OVERHEAD * actualUserOpFeePerGas);
+        if (fee > denomination) fee = denomination; // safety cap
+
+        uint256 remainder = denomination - fee;
+        (bool ok, ) = destination.call{value: remainder}("");
+        if (!ok) revert ForwardFailed();
+    }
 
     // ----- Internals -----
     function _withdrawFromTornado(
-        bytes calldata callData,
+        bytes memory proof,
+        bytes32 root,
+        bytes32 nullifierHash,
+        uint256 refund,
         bool execute
     ) internal {
         if (msg.sender != address(entryPoint())) revert CallerNotEntryPoint();
-        if (bytes4(callData[:4]) != WITHDRAW_FUNCTION_SELECTOR)
-            revert InvalidSelector();
-
-        (
-            bytes memory proof,
-            bytes32 root,
-            bytes32 nullifierHash,
-            address payable recipient,
-            uint256 refund
-        ) = abi.decode(
-                callData[4:],
-                (bytes, bytes32, bytes32, address, uint256)
-            );
-
         if (refund != 0) revert NonZeroRefund();
 
         // Execute the withdrawal on tornado
@@ -104,7 +121,7 @@ contract TornadoPaymaster is IAccount, BasePaymaster {
                 proof,
                 root,
                 nullifierHash,
-                recipient,
+                payable(address(this)), // recipient
                 payable(address(0)), // relayer
                 uint256(0), // fee
                 refund
@@ -123,7 +140,7 @@ contract TornadoPaymaster is IAccount, BasePaymaster {
             [
                 uint256(root),
                 uint256(nullifierHash),
-                uint256(uint160(address(recipient))),
+                uint256(uint160(address(this))), // recipient
                 uint256(0), // relayer
                 uint256(0), // fee
                 uint256(0) // refund
